1b326eb3f45e871bbe3e67e7985745db0918e127
Илья <camypai.13@list.ru>
2018-07-12 19:21:56 +0300

    Add functions for work with CKEditor

--- Portfoglio/wwwroot/js/site.js
+++ Portfoglio/wwwroot/js/site.js
@@ -1,13 +1,52 @@
 ﻿// Write your JavaScript code.
-$(".cardClick").on("click", function(){
+$(".cardClick").on("click", function () {
     UIkit.lightbox(this).show(1);
 });
 
-$("#checkbox").on("click", function(){
+$("#checkbox").on("click", function () {
     if ($(this).is(':checked')) {
         $("#hiddenCheckbox").val(true);
     }
-    else{
+    else {
         $("#hiddenCheckbox").val(false);
     }
+});
+
+var editor = CKEDITOR.replace('editor1');
+
+CKEDITOR.stylesSet.add( 'default', [
+    {
+        name: 'Price name',
+        element: 'div',
+        attributes: {
+            class: 'uk-width-expand',
+            'uk-leader': ''
+        }
+    },
+    {
+        name: 'Price value',
+        element: 'div'
+    }
+] );
+
+// CKEDITOR.stylesSet.add( 'pp1', [
+//     {
+//         name: 'Price value',
+//         element: 'div'
+//     }
+// ] );
+
+editor.on('change', function (evt) {
+    var dataText = evt.editor.getData();
+    $.ajax({
+        type: "POST",
+        url: "/AdminArt/HtmlEdit",
+        data: {
+            data: dataText,
+            target: $('textarea[name=editor1]').attr('id')
+        },
+        success: function (data) {
+            console.log(data);
+        }
+    });
 });
